c343f7fc0b62e631d781991f9e7f83624280981a
AureusInvictus <sabretempo@gmail.com>
2025-07-05 22:48:48 -0500

    Update firebase_utils to use Streamlit secrets for secure deployment

--- modules/utils/firebase_utils.py
+++ modules/utils/firebase_utils.py
@@ -1,18 +1,15 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
-import os
-
-FIREBASE_CRED_PATH = "firebase_credentials.json"
-FIREBASE_DB_URL = "https://aureusengine-433eb-default-rtdb.firebaseio.com/"
 
 firebase_app = None
 
 def initialize_firebase():
     global firebase_app
     if not firebase_admin._apps:
-        cred = credentials.Certificate(FIREBASE_CRED_PATH)
+        cred = credentials.Certificate(st.secrets["firebase"])
         firebase_app = firebase_admin.initialize_app(cred, {
-            'databaseURL': FIREBASE_DB_URL
+            'databaseURL': "https://aureusengine-433eb-default-rtdb.firebaseio.com/"
         })
 
 def sanitize_pattern(pattern_str):
@@ -37,9 +34,3 @@ def upload_user_pattern(pattern_str):
     if sanitized not in patterns:
         patterns.append(sanitized)
         ref.set(patterns)
-
-def retrieve_user_patterns():
-    initialize_firebase()
-    ref = db.reference("user_patterns")
-    patterns = ref.get()
-    return [sanitize_pattern(p) for p in patterns] if patterns else []
